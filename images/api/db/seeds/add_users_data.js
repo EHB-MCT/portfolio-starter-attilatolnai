70aebbbb7000bda36dd4df6f5c565420907f81d7
attilatolnai <attila.tolnai@student.ehb.be>
2023-11-29 14:27:16 +0100

    change: Added text to README
    Added text to the README file which was empty

--- images/api/db/seeds/add_users_data.js
+++ images/api/db/seeds/add_users_data.js
@@ -6,6 +6,30 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
 
   const users = await knex("users").select("*");
+  if (users.length === 0) {
+    // Seed the "users" table first if needed
+    await knex('users').insert([
+      { username: 'user1', password: 'password1' },
+      { username: 'user2', password: 'password2' },
+      { username: 'user3', password: 'password3' }
+    ]);
+  }
+
+  // Inserts seed entries for "info" table
+  return knex('info').insert([
+    { infoName: 'Attila-Info', description: 'Attila test', user_id: 1 },
+    { infoName: 'Jens-Info', description: 'Jens test', user_id: 2 },
+    { infoName: 'Mehdi-Info', description: 'Mehdi test', user_id: 3 }
+    // Add more entries as needed
+  ]);
+};
+
+/*
+
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+
+  const users = await knex("users").select("*");
   if(users.length == 0) {
       // Inserts seed entries
       return knex('users').insert([
@@ -15,4 +39,6 @@ exports.seed = async function(knex) {
       ]);
     };
 };
+
+*/
 

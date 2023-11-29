898f2fe8b006c5c82b956987a295dd2905111f65
attilatolnai <attila.tolnai@student.ehb.be>
2023-11-29 15:05:55 +0100

    update: corrected users table names
    changed the 'username' in the 'users' table to the correct values

--- images/api/db/seeds/add_users_data.js
+++ images/api/db/seeds/add_users_data.js
@@ -9,9 +9,9 @@ exports.seed = async function(knex) {
   if (users.length === 0) {
     // Seed the "users" table first if needed
     await knex('users').insert([
-      { username: 'user1', password: 'password1' },
-      { username: 'user2', password: 'password2' },
-      { username: 'user3', password: 'password3' }
+      { username: 'Attila', password: 'password1' },
+      { username: 'Jens', password: 'password2' },
+      { username: 'Mehdi', password: 'password3' }
     ]);
   }
 
